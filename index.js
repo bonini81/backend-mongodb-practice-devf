e1f14281f5fe7279461bea9c93c97692112c27be
bonini81 <andresdominguez81@gmail.com>
2020-05-03 20:53:38 -0500

    Agregue mas carpetas, como database, models, router y config de .env file

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-const express = require('express');
-const server = express();
-const PORT = process.env.PORT || 3000;
+require('dotenv').config()
 
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
+// 1) Obtenemos app y PORT mediante "destructuring"
+// 2) app y PORT vienen del archivo index de la carpeta server
+// NOTA: No es necesario agregar "index" cuando se solicita mediante 
+const { server, PORT } = require('./server');
+require('./database');
 
-// Endpoints
-server.get('/', (req, res) => res.send('Hello World!'));
-
-server.post('/api/v1/movies', (req, res) => res.status(201).send({ movies: [] }));
-
-server.get('/api/v1/temario/:id', (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
-  res.status(200).send("cool");
-});
-
-const Users = require('../models/Users');
-
-// CRUD Usuario
-
-// CREATE
-server.post('/api/v1/users', (req, res) => {
-  Users.create(req.body)
-    .then(user => res.status(201).send(user))
-    .catch(err => res.status(400).send({ message: 'Error creating user', err }));
-});
-
-// GET (ALL)
-server.get('/api/v1/users', (req, res) => {
-  Users.find()
-    .then(users => res.status(200).send(users))
-    .catch(err => res.status(404).send({ message: 'Users not found', err }));
-});
-
-// exportar server para poder requerirlo desde otros archivos
-module.exports = { server, PORT };
+// Encedemos servidor
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
--- server/index.js
+++ server/index.js
@@ -1,12 +1,38 @@
-const express = require('express')
-const server = express()
-const port = 3000
-//cambiar de app a server mejorar la parte de naming
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 3000;
 
-server.get('/', (req, res) => res.send('Hello World! Madafaka!'))
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
+// Endpoints
+server.get('/', (req, res) => res.send('Hello World!'));
 
+server.post('/api/v1/movies', (req, res) => res.status(201).send({ movies: [] }));
 
-//module.exports = app;
+server.get('/api/v1/temario/:id', (req, res) => {
+  console.log(req.query);
+  console.log(req.params);
+  res.status(200).send("cool");
+});
 
-server.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+const Users = require('../models/Users');
+
+// CRUD Usuario
+
+// CREATE
+server.post('/api/v1/users', (req, res) => {
+  Users.create(req.body)
+    .then(user => res.status(201).send(user))
+    .catch(err => res.status(400).send({ message: 'Error creating user', err }));
+});
+
+// GET (ALL)
+server.get('/api/v1/users', (req, res) => {
+  Users.find()
+    .then(users => res.status(200).send(users))
+    .catch(err => res.status(404).send({ message: 'Users not found', err }));
+});
+
+// exportar server para poder requerirlo desde otros archivos
+module.exports = { server, PORT };
