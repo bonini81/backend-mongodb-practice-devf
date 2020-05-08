ae2df1c477745e3e7631ad5af7f0117a29940edc
bonini81 <andresdominguez81@gmail.com>
2020-05-08 09:58:13 -0500

    First commit, added the controller folder on my gamer PC

--- router/UserRoutes.js
+++ router/UserRoutes.js
@@ -1,1 +1,10 @@
 /* Lo de routs */
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// Endpoints
+server.get('/', (req, res) => res.send('Hello World!'));
