17478d606cefff69abddc7c3c3a4f70af4d27e7c
bonini81 <andresdominguez81@gmail.com>
2020-05-10 20:49:51 -0500

    Bueno corregi el Server index, y ahi estaba el bug en el arranque del sistema

--- server/index.js
+++ server/index.js
@@ -5,32 +5,12 @@ const PORT = process.env.PORT || 3000;
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
-// Endpoints
+// Initial Endpoit
 server.get('/', (req, res) => res.send('Hello World! My man'));
 
 
-
-server.post('/api/v1/movies', (req, res) => res.status(201).send({ movies: [] }));
-
-server.get('/api/v1/temario/:id', (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
-  res.status(200).send("cool");
-});
-
-const Users = require('../models/Users');
-
-// CRUD Usuario
-
-// CREATE
-server.post('/api/v1/users', (req, res) => {
- 
-});
-
-// GET (ALL)
-server.get('/api/v1/users', (req, res) => {
- 
-});
+//Basicamente aqui pido el enrutador y desde ahi se delega lo mas, cada uno a lo suyo
+server.use('/api/v1',  require('../router'));
 
 // exportar server para poder requerirlo desde otros archivos
 module.exports = { server, PORT };
