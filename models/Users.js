fa88e9c24fddb2ae25997c99d80d16615a284610
bonini81 <andresdominguez81@gmail.com>
2020-05-18 18:44:17 -0500

    Instale Bcrypt, creando el script para usarlo en Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+//Below 2 Bcrypts dependencies
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+// End Bcrypt Dependencies
 
 const usersSchema = new Schema({
 
@@ -30,11 +34,49 @@ const usersSchema = new Schema({
     type: Boolean,
     default: true,
   },
+
+
 },
 {
   timestamps: true,
 });
 
+
+//Bcrypt PassWord Hasher Script
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+// only hash the password if it has been modified (or is new)
+if (!user.isModified('password')) return next();
+
+// generate a salt
+bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+  if (err) return next(err);
+
+  // hash the password using our new salt
+  bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+  });
+});
+
+
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
+
+// End Bcrypt Script
+
 const Users = mongoose.model('User', usersSchema);
 
 module.exports = Users;
+
